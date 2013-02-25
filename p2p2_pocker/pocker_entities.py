1c0a770f25ccfa4c291e3185b7fe058777e040a2
Cristina Botez <cristina_maria_botez@yahoo.com>
2013-02-25 19:27:52 +0200

    Added functionality to Dealer, Deck, Card

--- p2p2_pocker/pocker_entities.py
+++ p2p2_pocker/pocker_entities.py
@@ -1,17 +1,17 @@
 class Card(object):
-    
+
     def __init__(self, rank, suite):
         self._rank = rank
         self._suite = suite
-    
+
     @property
     def rank(self):
         return self._rank
-    
+
     @rank.setter
     def rank(self, value):
         self._rank = value
-    
+
     @property
     def suite(self):
         return self._suite
@@ -20,8 +20,51 @@ class Card(object):
     def suite(self, value):
         self._suite = suite
 
+    def __eq__(self, another):
+        return self._rank == another._rank and self._suite == another._suite
+
+    def __repr__(self):
+        return 'Card=(rank=%s,suite=%s)' % (self._rank, self._suite)
+
+
+class Deck(object):
+
+    def __init__(self):
+        self._cards = []
+        for suite in ['Diamonds', 'Hearts', 'Spades', 'Clubs']:
+            for rank in ['2', '3', '4', '5', '6', '7', '8',
+                         '9', '10', 'J', 'Q', 'K', 'A']:
+                self._cards.append(Card(rank, suite))
+
+    def popCard(self):
+        return self._cards.pop()
+
+    def __repr__(self):
+        return repr(self._cards)
+
+    def __len__(self):
+        return len(self._cards)
+
+
 class Dealer(object):
-    
-    def deal_player_hand(self):
-        return (Card('A', 'Diamonds'), Card('2', 'Spades'))
+
+    def __init__(self):
+        self._deck = Deck()
+
+    @property
+    def deck(self):
+        return self._deck
+
+    @deck.setter
+    def deck(self, value):
+        self._deck = value
+
+    def dealPlayerHand(self):
+        return (self.dealOneCard(), self.dealOneCard())
+
+    def dealTableHand(self):
+        return tuple([self.dealOneCard() for i in range(5)])
+
+    def dealOneCard(self):
+        return self._deck.popCard()
 
--- p2p2_pocker/test_texas_holdem_rules.py
+++ p2p2_pocker/test_texas_holdem_rules.py
@@ -3,27 +3,50 @@ import unittest
 from pocker_entities import Dealer, Card
 
 class TestTexasHoldemRules(unittest.TestCase):
-    
+
     def setUp(self):
         self._dealer = Dealer()
-        self._hand = self._dealer.deal_player_hand()
-    
+
     def tearDown(self):
         del self._dealer
-        del self._hand
-    
-    def testPlayerReceives2Cards(self):
-        self.assertEqual(2, len(self._hand))
+
+    def testTableHandReceives5Cards(self):
+        hand = self._dealer.dealTableHand()
+        self.assertEqual(5, len(hand))
+
+    def testTableHandReceivesValidCards(self):
+        hand = self._dealer.dealTableHand()
+        self._assertValidHand(5, hand)
+
+    def testPlayerHandReceives2Cards(self):
+        hand = self._dealer.dealPlayerHand()
+        self.assertEqual(2, len(hand))
 
     def testPlayerReceivesValidCards(self):
-        self._assertValidCard(self._hand[0])
-        self._assertValidCard(self._hand[1])
-    
+        hand = self._dealer.dealPlayerHand()
+        self._assertValidHand(2, hand)
+
     def testPlayerReceivesUniqueCards(self):
-        self.assertNotEqual(self._hand[0], self._hand[1])
-    
+        hand = self._dealer.dealPlayerHand()
+        self.assertNotEqual(hand[0], hand[1])
+
+    def testUniqueCardsAreDealtFromOneDeck(self):
+        dealtCards = []
+        for i in range(52):
+            dealtCards.append(self._dealer.dealOneCard())
+        self._assertUniqueCards(dealtCards)
+
+    def _assertUniqueCards(self, deck):
+        for i, card in zip(range(52), deck):
+            self.assertNotIn(card, deck[:i])
+
+    def _assertValidHand(self, handSize, hand):
+        for i in range(handSize):
+            self._assertValidCard(hand[i])
+
     def _assertValidCard(self, card):
-        self.assertIn(card.rank, ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'])
+        self.assertIn(card.rank, ['2', '3', '4', '5', '6', '7', '8',
+                                  '9', '10', 'J', 'Q', 'K', 'A'])
         self.assertIn(card.suite, ['Spades', 'Hearts', 'Diamonds', 'Clubs'])
 
 
