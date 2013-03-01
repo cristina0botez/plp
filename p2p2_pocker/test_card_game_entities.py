062149c6959e41849759d0b549d85866121a23e7
Cristina Botez <cristina_maria_botez@yahoo.com>
2013-03-01 17:49:50 +0200

    Added test cases for Hand

--- p2p2_pocker/test_card_game_entities.py
+++ p2p2_pocker/test_card_game_entities.py
@@ -1,14 +1,18 @@
 import unittest
 
-from card_game_entities import Deck
+from card_game_entities import Deck, Hand
 
-class TestCardGameEntities(unittest.TestCase):
+class TestDeck(unittest.TestCase):
 
     def setUp(self):
         suits = ['A', 'B']
         ranks = ['1', '2', '3', '4']
         self.deck1 = Deck(suits, ranks)
         self.deck2 = Deck(suits, ranks)
+
+    def tearDown(self):
+        del self.deck1
+        del self.deck2
 
     def test_shuffled_decks_of_same_type_are_not_equal(self):
         self.deck1.shuffle()
@@ -22,6 +26,26 @@ class TestCardGameEntities(unittest.TestCase):
         initial_size = len(self.deck1)
         self.deck1.pop_card()
         self.assertEqual(len(self.deck1), initial_size - 1)
+
+
+class TestHand(unittest.TestCase):
+
+    def setUp(self):
+        self.cards = [1, 2, 3]
+        self.hand = Hand(self.cards)
+
+    def tearDown(self):
+        del self.hand
+        del self.cards
+
+    def test_hand_contains_input_cards(self):
+        self.assertEqual(len(self.hand), len(self.cards))
+        for card_in_hand, card in zip(self.hand, self.cards):
+            self.assertEqual(card_in_hand, card)
+
+    def test_cards_in_hand_are_accessible_by_index(self):
+        for index in range(len(self.hand)):
+            self.assertIsNotNone(self.hand[index])
 
 
 if __name__ == '__main__':
